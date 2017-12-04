b7406a3ff5bd18935b4e7028437c333832e19f4d
wsknorth <wsknorth@protonmail.chx>
2017-12-04 16:18:04 +0100

    0.0.2

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const Telegraf = require('telegraf')
 
+const { js } = require('./lib/js')
+const { roll } = require('./lib/roll')
+
 const { configs } = require('./lib/configs')
 const { version, author } = require('./package.json')
-const { js } = require('./lib/js')
 
-const bot = new Telegraf(configs.token, { username: 'midnabot' })
+const bot = new Telegraf(configs.token, { username: configs.username })
 
 bot.command('js', js())
+bot.command('roll', roll())
 
 bot.command('version', (ctx) => ctx.replyWithMarkdown(`\`${version}\``))
 bot.command('author', (ctx) => ctx.replyWithMarkdown(`\`${author}\``))
