edb731535ec26dca0166eaab800b82d9fd86e047
wsknorth <wsknorth@protonmail.chx>
2017-12-04 18:35:10 +0100

    0.1.0

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const { js } = require('./lib/js')
 const { roll } = require('./lib/roll')
 
 const { configs } = require('./lib/configs')
-const { version, author } = require('./package.json')
+const { name, version, author, homepage } = require('./package.json')
 
 const bot = new Telegraf(configs.token, { username: configs.username })
 
 bot.command('js', js())
 bot.command('roll', roll())
 
-bot.command('version', (ctx) => ctx.replyWithMarkdown(`\`${version}\``))
-bot.command('author', (ctx) => ctx.replyWithMarkdown(`\`${author}\``))
+bot.command('about', (ctx) =>
+  ctx.replyWithMarkdown(`*${name} v.${version}*\n${author}\n${homepage}`))
 
 bot.startPolling()
