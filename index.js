c47496cff7abed5aeb4458819bb893a2b869e9d7
wsknorth <wsknorth@protonmail.chx>
2017-12-06 19:10:15 +0100

    refactoring

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 require('dotenv').config()
 
-const { telegraf } = require('./lib/telegraf')
+const Telegraf = require('telegraf')
+
 const { configs } = require('./lib/configs')
+const { sandbox } = require('./lib/sandbox')
+const { about } = require('./lib/about')
+const { roll } = require('./lib/roll')
+
+const bot = new Telegraf(configs.token, { username: configs.username })
+
+bot.command('sandbox', sandbox())
+bot.command('about', about())
+bot.command('roll', roll())
 
 const production = () => {
-  telegraf.telegram.setWebhook(configs.webhook)
-  telegraf.startWebhook('/', null, configs.port)
+  bot.telegram.setWebhook(configs.webhook)
+  bot.startWebhook('/', null, configs.port)
 }
 
 const development = () => {
-  telegraf.telegram.deleteWebhook()
-  telegraf.startPolling()
+  bot.telegram.deleteWebhook()
+  bot.startPolling()
 }
 
 (process.env.NODE_ENV === 'production')
